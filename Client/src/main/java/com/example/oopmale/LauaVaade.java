86a6c78eec49c5057b9af441853eaf0d6556c5b3
ma10-r <madis.roosma@gmail.com>
2026-04-05 21:49:32 +0300

    Heal juhul saab nüüd käike teha

--- Client/src/main/java/com/example/oopmale/LauaVaade.java
+++ Client/src/main/java/com/example/oopmale/LauaVaade.java
@@ -14,33 +14,66 @@ public class LauaVaade {
     private GridPane ruudustik = new GridPane();
     private Malelaud malelaud;
 
+    private Integer esimeneX = null;
+    private Integer esimeneY = null;
+
     public LauaVaade(Malelaud laud) {
         malelaud = laud;
         uuendaLaud();
+    }
+
+    private void ruutKlikiti(int x, int y) {
+        if (esimeneX == null) {
+            esimeneX = x;
+            esimeneY = y;
+            System.out.println("Esimene klikk: " + x + ", " + y);
+        } else {
+            int vanaX = esimeneX;
+            int vanaY = esimeneY;
+            int uusX = x;
+            int uusY = y;
+
+            System.out.println("Teine klikk: " + uusX + ", " + uusY);
+            System.out.println("Teen käigu: " + vanaX + "," + vanaY + " -> " + uusX + "," + uusY);
+
+            malelaud.teeKaik(vanaX, vanaY, uusX, uusY);
+            esimeneX = null;
+            esimeneY = null;
+
+            uuendaLaud();
+        }
     }
 
     private void ehitaLaud() {
         for (int rida = 0; rida < 9; rida++) {
             for (int veerg = 0; veerg < 9; veerg++) {
                 if (veerg == 0 && rida == 0) {
+                    // top-left empty corner
                 } else if (rida == 0) {
-                    Label täht = new Label(Character.toString((char) ('A' + veerg-1)));
+                    Label täht = new Label(Character.toString((char) ('A' + veerg - 1)));
                     täht.setMaxWidth(Double.MAX_VALUE);
                     täht.setAlignment(Pos.CENTER);
                     ruudustik.add(täht, veerg, rida);
                 } else if (veerg == 0) {
-                    Label number = new Label(Integer.toString(9-rida));
+                    Label number = new Label(Integer.toString(9 - rida));
                     number.setMinWidth(30);
                     number.setMaxWidth(Double.MAX_VALUE);
                     number.setAlignment(Pos.CENTER);
                     ruudustik.add(number, veerg, rida);
                 } else {
                     Rectangle ruut = new Rectangle(80, 80);
+
                     if ((rida + veerg) % 2 == 0) {
                         ruut.setFill(Color.WHITE);
                     } else {
                         ruut.setFill(Color.GREEN);
                     }
+
+                    int x = veerg - 1;
+                    int y = 8 - rida;
+
+                    ruut.setOnMouseClicked(e -> ruutKlikiti(x, y));
+
                     ruudustik.add(ruut, veerg, rida);
                 }
             }
@@ -77,7 +110,10 @@ public class LauaVaade {
     }
 
     public void uuendaLaud() {
+        ruudustik.getChildren().clear();
         ehitaLaud();
-        malelaud.getKoikNupud().forEach(malend -> ruudustik.add(getPilt(malend), malend.getX()+1, 8-malend.getY()));
+        malelaud.getKoikNupud().forEach(
+                malend -> ruudustik.add(getPilt(malend), malend.getX() + 1, 8 - malend.getY())
+        );
     }
 }
