8250944824d4f4c9f552c1cb49ade2ce626ede3f
kirkekt <kirkekarolintark@gmail.com>
2026-04-25 21:23:40 +0300

    kontroll, kas nuppu võimalik liigutada sihtruudule + tule all olemise kontroll

--- Server/src/main/java/org/server/Malelaud.java
+++ Server/src/main/java/org/server/Malelaud.java
@@ -39,22 +39,79 @@ public class Malelaud {
      */
     public boolean kasLubatudKaik(boolean valgeKaik, int[] kaik){
         Asukoht vanaAsukoht = new Asukoht(kaik[0], kaik[1]);
-        Asukoht deltaKaik = new Asukoht(kaik[2]-kaik[0], kaik[3]-kaik[1]);
         Asukoht uusAsukoht = new Asukoht(kaik[2], kaik[3]);
 
         Malenupp liigutatavNupp = misNuppRuudul(vanaAsukoht);
-        Malenupp sihtNupp = misNuppRuudul(uusAsukoht);
+        if (liigutatavNupp == null)
+            return false;
 
-        if (liigutatavNupp == null
-                || liigutatavNupp.onValge() != valgeKaik
-                || !liigutatavNupp.kaiguDeltad().stream().anyMatch(elt -> elt.contains(deltaKaik))
-                || sihtNupp != null && sihtNupp.onValge() == valgeKaik
-        )  return false;
+        return voimalikLiigutada(liigutatavNupp, uusAsukoht);
+    }
 
-        // kas teekond on vaba?
-        if (!kasTeekondVaba(vanaAsukoht, uusAsukoht)) return false;
+    /**
+     * Vaatab, kas nuppu on võimalik liigutada sihtruudule. Ei kontrolli, kas nupp on tule all või
+     * kas liigutuse tulemus paneb kuninga tule alla
+     * @param nupp
+     * @param sihtAsukoht
+     * @return
+     */
+    public boolean voimalikLiigutada(Malenupp nupp, Asukoht sihtAsukoht) {
+        Asukoht algus = nupp.getAsukoht();
+        Asukoht deltaKaik = new Asukoht(sihtAsukoht.getX() - algus.getX(), sihtAsukoht.getY() - algus.getY());
+
+        List<Asukoht> oigeSuunaDeltad = null;
+        for (List<Asukoht> suunaDeltad : nupp.kaiguDeltad()) {
+            if (suunaDeltad.contains(deltaKaik)) {
+                oigeSuunaDeltad = suunaDeltad;
+                break;
+            }
+        }
+
+        if (oigeSuunaDeltad == null)
+            return false; // Nupul võimatu sellist käiku teha
+
+        for (Asukoht vaadeldavDelta : oigeSuunaDeltad) {
+            Asukoht vaadeldavAsukoht = new Asukoht(algus.getX() + vaadeldavDelta.getX(), algus.getY() + vaadeldavDelta.getY());
+            if (vaadeldavAsukoht.equals(algus))
+                continue;
+
+            Malenupp nuppRuudul = misNuppRuudul(vaadeldavAsukoht);
+            if (nuppRuudul != null) {
+                if (vaadeldavAsukoht.equals(sihtAsukoht)) {
+                    return nuppRuudul.onValge() != nupp.onValge(); // Lõppruudul peab olema teist värvi nupp
+                }
+                return false;
+            }
+        }
 
         return true;
+    }
+
+    /**
+     * Kontrollib, kas vaadeldav mängija on tule all
+     * @param valgeKaik
+     * @return
+     */
+    public boolean onTuli(boolean valgeKaik) {
+        Malenupp kuningas = null;
+        List<Malenupp> mangijaNupud = valgeKaik ? valgedNupud : mustadNupud;
+        List<Malenupp> vastaseNupud = valgeKaik ? mustadNupud : valgedNupud;
+
+        for (Malenupp nupp: mangijaNupud) {
+            if (nupp.getClass() == Kuningas.class) {
+                kuningas = nupp;
+            }
+        }
+
+        if (kuningas == null)
+            throw new RuntimeException("Laual ei ole kuningat");
+
+        for (Malenupp nupp : vastaseNupud) {
+            if (voimalikLiigutada(nupp, kuningas.getAsukoht())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private Malenupp misNuppRuudul(Asukoht asukoht) {
@@ -64,59 +121,6 @@ public class Malelaud {
             }
         }
         return null;
-    }
-
-    private boolean kasTeekondVaba(Asukoht a, Asukoht b) {
-        Asukoht algus, lõpp;
-        if (a.getX() < b.getX()) {
-            algus = a;
-            lõpp = b;
-        } else if (b.getX() < a.getX()) {
-            algus = b;
-            lõpp = a;
-        } else {
-            if (a.getY() > b.getY()) {
-                algus = b;
-                lõpp = a;
-            } else {
-                algus = a;
-                lõpp = b;
-            }
-        }
-
-        // horisontaalne
-        if (algus.getY() == lõpp.getY()) {
-            for (int x = algus.getX() + 1; x < lõpp.getX(); x++) {
-                if (misNuppRuudul(new Asukoht(x,lõpp.getY())) != null) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        // vertikaalne
-        if (algus.getX() == lõpp.getX()) {
-            for (int y = algus.getY() + 1; y < lõpp.getY(); y++) {
-                if (misNuppRuudul(new Asukoht(lõpp.getX(),y)) != null) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        // diagonaalne
-        for (int x = algus.getX() + 1; x < lõpp.getX(); x++) {
-            int muut = x - algus.getX();
-
-            if (algus.getY() > lõpp.getY()) {
-                muut = -muut;
-            }
-
-            if (misNuppRuudul(new Asukoht(x, algus.getY() + muut)) != null) {
-                return false;
-            }
-        }
-        return true;
     }
 
     /**
